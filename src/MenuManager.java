9c149df503fbfa80055ecc4d25c0653c3ff894c0
junghoon <gkwjdgns7@naver.com>
2021-03-27 22:45:40 +0900

    menu selection with methods

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -5,8 +5,9 @@ public class MenuManager {
 	public static void main(String[] args) {
 		int num = 0;
 		Scanner input = new Scanner(System.in);
-		
+
 		while (num != 6) {
+			System.out.println("*** Friends Management System Menu ***");
 			System.out.println("1. Add friends");
 			System.out.println("2. Delete friends");
 			System.out.println("3. Edit friends");
@@ -15,40 +16,54 @@ public class MenuManager {
 			System.out.println("6. Exit");
 			System.out.println("Select on number between 1-6:");
 			num = input.nextInt();
-			switch(num) {
-			case 1:
-				System.out.print("Friends number :");
-				int friendsNumber = input.nextInt();
-				System.out.print("Friends name : ");
-				String friendsName = input.next();
-				break;
-			case 2:
-				System.out.print("Friends number: ");
-				int friendsNumber2 = input.nextInt();
-				input.reset();
-				break;
-				
-			case 3:
-				System.out.print("Friends number: " );
-				int friendsNumber3 = input.nextInt();
-				input.reset();
-				int friendsNumber4 = input.nextInt();
-				System.out.print("Friends name: " );
-				String friendsName2 = input.next();
-				input.reset();
-				String friendsName3 = input.next();
-				
-				break;
-				
-			case 4:
-				System.out.print("Friends number:");
-				System.out.print("Friends name: ");
-				break;
-			case 5:
-				break;
+			if (num == 1) {
+				addFriends();
 			}
-				
+			else if (num == 2) {
+				deleteFriends();
+			}
+			else if (num == 3) {
+				editFriends();
+			}
+			else if (num == 4) {
+				viewFriends();
+			}
+			else {
+				continue;
+			}
 		}
+	}
+
+	public static void addFriends() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Friends ID :");
+		int FriendsId = input.nextInt();
+		System.out.print("Friends name :");
+		String FriendsName = input.next();
+		System.out.print("Email address :");
+		String FriendsEmail = input.next();
+		System.out.print("Phone number :");
+		String FriendsPhone = input.next();
+	}
+	public static void deleteFriends() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Friends ID :");
+		int FriendsId = input.nextInt();
+
+	}
+	
+	public static void editFriends() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Friends ID :");
+		int FriendsId = input.nextInt();
+
+	}
+	
+	public static void viewFriends() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Friends ID :");
+		int FriendsId = input.nextInt();
+
 	}
 
 }
